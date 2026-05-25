627201f73d4bbfffa018ba04876bd11159bd590e
andreiyopt <drei.in.mc@gmail.com>
2026-05-25 22:33:37 +0900

    tests with .getignored and .env"

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('Hello from repository!')
+
+def print_author():
+# Допишите здесь ваш код
+    from dotenv import load_dotenv
+    import os
+    load_dotenv(dotenv_path='/Users/user/Documents/jupyter/Практикум/Лекции/8_terminal/project/.env')
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+print_author()
+
